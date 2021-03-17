ea7f64c9273cfba824848b4983c463674ecc82c1
Megumikawa <megumi19901016@gmail.com>
2021-03-17 21:49:30 +0100

    add route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+// import React, { Component } from "react";
+import {Route, Switch} from 'react-router-dom';
+// import Home from './components/Home';
 
 function App() {
   return (
     <div>
+      <Switch>
+        <Route path="/" />
+      </Switch>
     </div>
   );
 }
