0747f45d98df0722f84a2c311ff0c3e1ec4caf51
Megumikawa <megumi19901016@gmail.com>
2021-03-18 22:36:58 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-// import React, { Component } from "react";
+import React, { Component } from "react";
 import {Route, Switch} from 'react-router-dom';
 // import Home from './components/Home';
 
