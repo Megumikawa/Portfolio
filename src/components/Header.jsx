69e84922f8935bbcf8da0fa9ce9c20c2f1f962c9
Megumikawa <megumi19901016@gmail.com>
2021-04-19 21:19:15 +0200

    add header list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-      
+      <Link to="/"></Link>
+      <nav>
+        <ul>
+          <li>ABOUT</li>
+          <li>PROJECTS</li>
+          <li>CONTACT</li>
+        </ul>
+      </nav>
     </div>
   )
 }
