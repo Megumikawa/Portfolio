8fea68df30e1ec62e0eec4b69f698bb27bc3459d
Megumikawa <megumi19901016@gmail.com>
2021-04-21 22:00:20 +0200

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      <small>Megumik Portfolio</small>
+      <small>&copy; Megumik Portfolio</small>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <div>
+      <Link></Link>
       <nav>
         <ul>
           <li>ABOUT</li>
