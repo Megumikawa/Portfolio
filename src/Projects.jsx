cdf4ae45148cc22e6211d200ce3d2dcfd7a1c318
Megumikawa <megumi19901016@gmail.com>
2021-04-27 18:53:53 +0200

    add

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Projects() {
   return (
     <div>
-      
+      <ul>
+        <li>React app comming here</li>
+        <li>Node.js project comming here</li>
+        <li>game project comming here</li>
+      </ul>
     </div>
   )
 }
