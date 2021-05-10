e203b226d303c0a32a097afa3a70cd6e583be91a
Megumikawa <megumi19901016@gmail.com>
2021-05-10 20:05:42 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from "react";
 import {Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
