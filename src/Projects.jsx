4060e5c1eecf6df6eb72c390dddfcd104c827481
Megumikawa <megumi19901016@gmail.com>
2021-05-16 12:17:03 +0200

    add project of feuill

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -16,7 +16,12 @@ function Projects() {
       <section className="project-wrapper">
         <h1 className="project-title">Coin Collector</h1>
         <div className="project-link"></div>
-        <p>Kittchat is a online booking website of cat cafe in Berlin.</p>
+        <p>Coin colloctor is a simple coin game. A character move left, right, top and buttom by using arrow key.</p>
+      </section>
+      <section className="project-wrapper">
+        <h1 className="project-title">feiul</h1>
+        <div className="project-link"></div>
+        <p>feiull is a EC site of fashion accessories. </p>
       </section>
     </div>
   )
