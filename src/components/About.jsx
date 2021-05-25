52212be797352cd7f6cfbf717b358b31be5e48db
Megumikawa <megumi19901016@gmail.com>
2021-05-25 23:43:02 +0200

    add some sentences of about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 const About = () => {
   return (
     <>
-      <h1>About</h1>
-      <p>I am Megumi Kawagoe...........</p>
+      <h1>ABOUT</h1>
+      <p>Hello. I am Megumi Kawagoe from Japan, currently based in Amsterdam, Netherlands.</p>
+      <p>I started web development very begining from HTML, CSS, jQuery and WordPress in 2020.</p>
       <h1>Hobbies</h1>
-      <p>Hobbies are ...</p>
+      <ul>
+        <li>Yoga</li>
+        <li>Jogging</li>
+        <li>Traveling</li>
+        <li>Playing the piano</li>
+        <li>Japanese calligraphy</li>
+        <li>Handicraft</li>
+      </ul>
     </>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,8 @@ function Home() {
   return (
     <>
       <h1>This is a Home page</h1>
+      <h1>Hello. I am Megumi Kawagoe.</h1>
+      <h2>I am a Frontend Developer</h2>
       <p>some comes</p>
     </>
   )
