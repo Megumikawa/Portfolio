ba7e57be5f8049a7cc636c66e2b4d9cc87269b03
Megumikawa <megumi19901016@gmail.com>
2021-05-26 12:52:36 +0200

    add sentence of about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,16 +4,20 @@ const About = () => {
   return (
     <>
       <h1>ABOUT</h1>
-      <p>Hello. I am Megumi Kawagoe from Japan, currently based in Amsterdam, Netherlands.</p>
-      <p>I started web development very begining from HTML, CSS, jQuery and WordPress in 2020.</p>
+      <p>Hi There! I am Megumi Kawagoe based in Amsterdam, Netherlands.</p>
+      <p>I'm a passionate Frontend developer, born and brought up in Japan. I started coding from </p>
+      <p>I started coding from HTML, CSS,  jQuery and WordPress at Manabito.co in 2020.Through learning jQuery, I was eager to learn from fundamental to wide range of JavaScript. Then I decided to move into Europe and joined at Ironhack Fullstack Web Development Bootcamp in 2021.</p>
+      <p>I like working with team, especially whom have different background. So every moment of experiences at Bootcamp was fun, intense and very stimulating. </p>
+      {/* <p>Because I always put myself to step out from comfort zone, being passionate about web design and JavaScript and my diligent of learning new skills are never ending.</p> */}
+      
       <h1>Hobbies</h1>
       <ul>
+        <li>Traveling</li>
         <li>Yoga</li>
         <li>Jogging</li>
-        <li>Traveling</li>
-        <li>Playing the piano</li>
-        <li>Japanese calligraphy</li>
         <li>Handicraft</li>
+        <li>Japanese calligraphy</li>
+        <li>Playing the piano</li>
       </ul>
     </>
   )
