19ab621ce7c758164567e84154ce5aaf37042aa2
Megumikawa <megumi19901016@gmail.com>
2021-06-11 21:30:10 +0200

    change style of project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -45,7 +45,7 @@ function Projects() {
         </div>
           <Card className="my-project-card">
           <CardContent>
-            <h2 className="project-title">Kittchat</h2>
+            <h2 className="project-title">Kittchat</h2><hr />
             <p className="project-description">
             Kitchat is a cat cafe website that makes a reservation by choosing from 5 different locations in Berlin.<br/>
             Once you sign up, you can manage and check reservation history from your personal account.
@@ -87,9 +87,9 @@ function Projects() {
         </div>
         <Card className="my-project-card">
           <CardContent>
-            <h2 className="project-title">Animeek</h2>
+            <h2 className="project-title">Animeek</h2><hr />
             <p className="project-description">
-            Animeek is an app where you can search for your favorite Anime and be able to add it to your Watchlist. <br />
+            Animeek is an app where you can search for your favorite anime and be able to add it to your watchlist. <br />
             You just type any anime you desire and Animeek will give you all the information you need.
             </p>
             <div className="tech-skills">
@@ -125,7 +125,7 @@ function Projects() {
         </div>
         <Card className="my-project-card">
           <CardContent>
-            <h2 className="project-title">Coin Collector</h2>
+            <h2 className="project-title">Coin Collector</h2><hr />
             <p className="project-description">
             Coin-Collector is a game about collecting two types of coins which fall from top to bottom.<br />
             An enemy of the devil is also coming from the right side to reduce your points.<br />
@@ -167,19 +167,20 @@ function Projects() {
         </div>
         <Card className="my-project-card">
           <CardContent>
-            <h2 className="project-title">feuille</h2>
+            <h2 className="project-title">feuille</h2><hr />
             <p className="project-description">
-            This is the landing page of Feuille which is a jewelry E-commerce website.
+            This is a jewelry E-commercethe website landing page.<br />
+            You can check the item detail when you click an image and when you scroll down, you can check shop detail with Google Maps.
             </p>
-            <div className="project-link">
-              <a href="https://megumikawa.github.io/manabito-feuille/"><LanguageIcon />Web site</a>
-              <a href="https://github.com/Megumikawa/manabito-practice/tree/feature/feuille/feuille"><GitHubIcon />Repository</a>
-            </div>
             <div className="tech-skills">
               <span>#HTML5</span>
               <span>#CSS3</span>
               <span>#jQuery</span>
               <span>#Responsive</span>
+            </div>
+            <div className="project-link">
+              <a href="https://megumikawa.github.io/manabito-feuille/"><LanguageIcon />Checkout</a>
+              <a href="https://github.com/Megumikawa/manabito-practice/tree/feature/feuille/feuille"><GitHubIcon />Repository</a>
             </div>
           </CardContent>
         </Card>
