d4c539f4e540b3860ea3604c42fbd4c4674d5c7f
Megumikawa <megumi19901016@gmail.com>
2021-06-12 20:57:02 +0200

    change something small

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Header from './components/Header';
 
-// import ProjectsTest from './pages/ProjectsTest';
-
 function App() {
   return (
     <div className="main">
@@ -19,7 +17,6 @@ function App() {
         <Route path="/about" component={About} />
         <Route path="/projects" component={Projects} />
         <Route path="/contact" component={Contact} />
-        {/* <Route path="/projectstest" component={ProjectsTest} /> */}
       </Switch>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Kittchat from '../components/Kittchat'
-import Animeek from '../components/Animeek'
-import Coingame from '../components/Coingame'
-import Feuille from '../components/Feuille'
+import React from 'react';
+import Kittchat from '../components/Kittchat';
+import Animeek from '../components/Animeek';
+import Coingame from '../components/Coingame';
+import Feuille from '../components/Feuille';
+import Background from '../components/Background';
 
 
 function ProjectsTest() {
   return (
     <>
-    {/* <Background /> */}
-    <div id="projects" className="projects">
-      <h1>Projects</h1>
-      <Kittchat />
-      <Animeek />
-      <Coingame />
-      <Feuille />
-    </div>
+      <Background />
+      <div id="projects" className="projects">
+        <h1>Projects</h1>
+        <Kittchat />
+        <Animeek />
+        <Coingame />
+        <Feuille />
+      </div>
     </>
   )
 }
