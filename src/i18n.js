46c1e9d61bbcafd4ab869172dcfa2b221dc84b1f
Megumikawa <megumi19901016@gmail.com>
2021-07-07 15:03:41 +0200

    change skills

--- src/i18n.js
+++ src/i18n.js
@@ -4,19 +4,16 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from "react-i18next";
 import HttpApi from 'i18next-http-backend';
 
-
-
-
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .use(HttpApi)
   .init({
-    fallbackLng: ['en', 'de', 'ja'],
+    fallbackLng: ['en', 'ja'],
     debag: true,
     detection: {
-      order: ['cookie', 'path',  'htmlTag', 'localStorage', 'subdomain'],
+      order: ['cookie', 'path', 'htmlTag', 'localStorage', 'subdomain'],
       caches: ['cookie'],
     },
     interpolation: {
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,39 +3,24 @@ import React from 'react'
 const Skills = () => {
   return (
     <div id="skills" className="skills">
-      <h2>Skills</h2>
+      <h2>Skills</h2><hr />
         <div className="skill-container">
-          <section className="skill-topic">
-            <h3>Front End</h3>
-            <hr />
-              <ul className="skills-frontend">
-                <li>HTML5</li>
-                <li>CSS3</li>
-                <li>JavaScript</li>
-                <li>React.js</li>
-                <li>jQuery</li>
-                <li>WordPress</li>
-                <li>RESTAPI</li>
-                <li>Bootstrap</li>
-                <li>Material-UI</li>
-                <li>Responsive Design</li>
-              </ul>
-            </section>
-            <section className="skill-topic">
-              <h3>BackEnd</h3>
-              <hr />
-                <ul>
-                  <li>Node.js</li>
-                  <li>Express</li>
-                  <li>MongoDB</li>
-                </ul>
-              <h3>Version Control</h3>
-              <hr />
-                <ul>
-                  <li>Git</li>
-                  <li>Github</li>
-                </ul>
-            </section>
+          {/* <section className="skill-topic"> */}
+            <ul className="skills-frontend">
+              <li>JavaScript</li>
+              <li>ReactJS</li>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>APIs</li>
+              <li>MongoDB</li>
+              <li>Mongoose</li>
+              <li>WordPress</li>
+              <li>jQuery</li>
+              <li>HTML5</li>
+              <li>CSS3</li>
+              <li>Bootstrap</li>
+            </ul>
+          {/* </section> */}
         </div>
     </div>
   )
