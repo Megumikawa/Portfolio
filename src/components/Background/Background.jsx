371333da76cad5b0d1ca14533604ada05c1b3e49
Megumikawa <megumi19901016@gmail.com>
2021-07-19 23:32:02 +0200

    relocated background css to new file

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Particles from 'react-particles-js';
 import { particlesOptions } from "./ParticlesOptions";
+import './Background.css';
 
 function Background() {
   return (
