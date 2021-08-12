a9885dd80aeed8e00053146933ed793a234a86c9
Megumikawa <megumi19901016@gmail.com>
2021-08-12 16:17:00 +0200

    change style for maxwidth 479px

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -12,8 +12,8 @@ const Contact = () => {
       <h2>Get in touch!!</h2>
       <div className="contact-link">
         <a href="https://www.linkedin.com/in/megumi-kawagoe-88j/" rel="noreferrer" target="_blank"><Button className="contact-link-btn" variant="outline-primary"><LinkedInIcon />LinkedIn</Button></a>
-        <a href="https://github.com/Megumikawa" rel="noreferrer" target="_blank"><Button className="contact-link-btn"  variant="outline-dark"><GitHubIcon />Github</Button></a>
-        <a href="mailto:megumikawagoe@gmail.com"><Button className="contact-link-btn" variant="outline-success"><EmailIcon />Email</Button></a>
+        <a href="https://github.com/Megumikawa" rel="noreferrer" target="_blank"><Button className="contact-link-btn github-btn" variant="outline-dark"><GitHubIcon />Github</Button></a>
+        <a href="mailto:megumikawagoe@gmail.com"><Button className="contact-link-btn" variant="outline-success email-btn"><EmailIcon />Email</Button></a>
       </div>
     </div>
   )
