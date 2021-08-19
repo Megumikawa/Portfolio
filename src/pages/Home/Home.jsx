fe5b4a40d45c53bd7ada15d9534772c553f6a21a
Megumikawa <megumi19901016@gmail.com>
2021-08-19 21:23:09 +0200

    reback to original responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,19 +3,15 @@ import Typewriter from "typewriter-effect";
 import Background from '../../components/Background/Background';
 import { useTranslation } from 'react-i18next';
 import './Home.css'
-import Particles from 'react-particles-js';
-import { particlesOptions } from "../../components/Background/ParticlesOptions";
-import '../../components/Background/Background.css';
 
 
 function Home() {
   const { t, i18n } = useTranslation();
 
   return (
-    <div id="home" >
-      {/* <Background /> */}
-      <section className="home background particles">
-      {/* <Particles className="particles" params={particlesOptions} /> */}
+    <div id="home">
+      <Background />
+      <section className="home">
         <h1>Hi I'm<br />
           <span className="name">Megumi Kawagoe</span>
         </h1>
