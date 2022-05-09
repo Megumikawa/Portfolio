54af6351b07238d5b51bb3797143e198880a751f
Megumikawa <megumi19901016@gmail.com>
2022-05-09 15:31:23 +0200

    add the new project component base

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import KittchatProject from './KittchatProject';
-import AnimeekProject from './AnimeekProject';
-import CoingameProject from './CoingameProject';
-import FeuilleProject from './FeuilleProject';
-import CyptoProject from './CyptoProject';
-import TodoProject from './TodoProject';
+// import * as React from 'react';
+
+// import KittchatProject from './KittchatProject';
+// import AnimeekProject from './AnimeekProject';
+// import CoingameProject from './CoingameProject';
+// import FeuilleProject from './FeuilleProject';
+// import CyptoProject from './CyptoProject';
+// import TodoProject from './TodoProject';
 import ProjectsBackground from './ProjectsBackground';
 import './Projects.css';
+// import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+// import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Projects() {
   return (
@@ -14,15 +24,40 @@ function Projects() {
       <ProjectsBackground />
       <div id="projects" className="projects">
         <h1>Projects</h1>
-          <div className="projects-outline">
-          <KittchatProject />
+      
+      <Card sx={{ maxWidth: 345 }}>
+      
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">Cat cafe booking app</Typography>
+        <Typography variant="body2" color="text.secondary">#React #React #React</Typography>
+        </CardContent>
+        <CardActions>
+            {/* <Button size="small"> startIcon={<GitHubIcon />}</Button> */}
+            {/* <GitHubIcon /> */}
+        <Button size="small">github</Button>
+        <Button size="small">Link</Button>
+      </CardActions>
+        <CardMedia
+        component="img"
+        height="200"
+        image="images/kittchat1.png"
+        alt="green iguana"
+      />
+      
+        </Card>
+        </div>
+
+
+
+
+
+          {/* <KittchatProject />
           <AnimeekProject />
           <CoingameProject />
           <CyptoProject />
           <TodoProject />
-          <FeuilleProject />
-        </div>
-      </div>
+          <FeuilleProject /> */}
+        
     </>
   )
 }
